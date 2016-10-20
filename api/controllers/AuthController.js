d031f8bd43497087333452ba48867d41bc955883
Robophil <oghenerobo@gmail.com>
2016-10-20 12:49:07 +0100

    added code from fork

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,6 @@
+var bcrypt = require('bcrypt');
+var helper = require('../../lib/helper.js');
+
+module.exports = {
+  
+};
---
+++ api/models/User.js
@@ -0,0 +1,33 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+var bcrypt = require('bcrypt');
+
+module.exports = {
+
+  attributes: {
+    email: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: 'string',
+      required: true
+    },
+
+    active: {
+      type: 'boolean',
+      defaultsTo: true
+    },
+
+    isPasswordValid: function (password, cb) {
+      bcrypt.compare(password, this.password, cb);
+    }
+  }
+};
+
