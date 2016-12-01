119824197d885c866708005e9b366fe65f2ee4c3
Robophil <oghenerobo@gmail.com>
2016-12-01 11:55:12 +0100

    updated test

--- test/basic.js
+++ test/basic.js
@@ -1,47 +1,112 @@
 
 var Sails = require('sails').Sails;
+var request = require('supertest');
 
- describe('Basic tests ::', function() {
+var formData = {
+    email: 'test@test.com', password: '1234567', confirmPassword: '1234567'
+};
 
-     // Var to hold a running sails app instance
-     var sails;
+describe('Sails-hook-jsonwebtoken test ::', function () {
 
-     // Before running any tests, attempt to lift Sails
-     before(function (done) {
+    // Var to hold a running sails app instance
+    var sails, token1, token2, user;
 
-         // Hook will timeout in 10 seconds
-         this.timeout(11000);
+    // Before running any tests, attempt to lift Sails
+    before(function (done) {
 
-         // Attempt to lift sails
-         Sails().lift({
-           hooks: {
-             // Load the hook
-             "sails-hook-jsonwebtoken": require('../'),
-             // Skip grunt (unless your hook uses it)
-             "grunt": false
-           },
-           log: {level: "error"}
-         },function (err, _sails) {
-           if (err) return done(err);
-           sails = _sails;
-           return done();
-         });
-     });
+        // Hook will timeout in 10 seconds
+        this.timeout(13000);
 
-     // After tests are complete, lower Sails
-     after(function (done) {
+        // Attempt to lift sails
+        Sails().lift({
+            hooks: {
+                // Load the hook
+                "sails-hook-jsonwebtoken": require('../'),
+                // Skip grunt (unless your hook uses it)
+                "grunt": false
+            },
+            log: { level: "info" }
+        }, function (err, _sails) {
+            if (err) return done(err);
+            sails = _sails;
+            return done();
+        });
+    });
 
-         // Lower Sails (if it successfully lifted)
-         if (sails) {
-             return sails.lower(done);
-         }
-         // Otherwise just return
-         return done();
-     });
+    // After tests are complete, lower Sails
+    after(function (done) {
 
-     // Test that Sails can lift with the hook in place
-     it ('sails does not crash', function() {
-         return true;
-     });
+        // Lower Sails (if it successfully lifted)
+        if (sails) {
+            return sails.lower(done);
+        }
+        // Otherwise just return
+        return done();
+    });
 
- });
+
+    /**
+     * Let the test begin
+     */
+
+    describe('sails lift', function () {
+        // Test that Sails can lift with the hook in place
+        it('sails does not crash', function () {
+            return true;
+        });
+    })
+
+    describe('sign up', function () {
+        it('account created', function (done) {
+            request(sails.hooks.http.app)
+                .post('/jwt/signup')
+                .send(formData)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    user = res.body.user
+                    return done();
+                });
+        })
+    })
+
+    describe('login', function () {
+        it('first attempt', function (done) {
+            request(sails.hooks.http.app)
+                .post('/jwt/auth')
+                .send(formData)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    token1 = res.body.token;
+                    return done();
+                });
+        })
+
+        it('second attempt', function (done) {
+            request(sails.hooks.http.app)
+                .post('/jwt/auth')
+                .send(formData)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    token2 = res.body.token;
+                    return done();
+                });
+        })
+    })
+
+    describe('token check if still valid', function () {
+        it('same tokens are returned on succesive login ', function (done) {
+            if(token1 === token2) return done()
+            else return done("tokens do not match")
+        })
+    })
+
+    describe('clean up', function () {
+        it('delete account created', function (done) {
+            User.destroy({id: user.id}).then(data => done()).catch(error => done(error))
+        })
+    })
+
+});
