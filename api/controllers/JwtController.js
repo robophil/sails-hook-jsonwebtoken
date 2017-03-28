c149405aadcee2e37c8348098d838e8672b360ed
Robophil <oghenerobo@gmail.com>
2017-03-28 05:55:44 +0100

    updated service and controller to handle signin/signup after operation new default value multiple account type

--- api/controllers/JwtController.js
+++ api/controllers/JwtController.js
@@ -1,19 +1,41 @@
 var bcrypt = require('bcryptjs');
 
 module.exports = {
+    /**
+     * Controller action called when authenticating a user
+     */
     auth: function (req, res) {
-        // Validate request paramaters
-        if (!req.body.email || !req.body.password) {
-            return res.json(401, {
-                err: {
-                    status: 'danger',
-                    message: 'Email or password not found'
-                }
-            });
+        var findCriteria = {}
+
+        //Checks if email/username and password is found
+        if (sails.config.jsonWebToken.authType == "email") {
+            // Validate request paramaters. If email and password exist
+            if (!req.body.email || !req.body.password) {
+                return res.json(401, {
+                    err: {
+                        status: 'danger',
+                        message: 'Email or password not found'
+                    }
+                });
+            }
+
+            findCriteria["email"] = req.body.email
+        } else {
+            // Validate request paramaters. If username and password exist
+            if (!req.body.username || !req.body.password) {
+                return res.json(401, {
+                    err: {
+                        status: 'danger',
+                        message: 'Username or password not found'
+                    }
+                });
+            }
+
+            findCriteria["username"] = req.body.username
         }
 
         //find email matching user
-        User.findOneByEmail(req.body.email).then((user) => {
+        User.findOne(findCriteria).then((user) => {
             if (!user) {
                 return res.json(401, {
                     err: {
@@ -27,7 +49,9 @@ module.exports = {
             user.isPasswordValid(req.body.password, function (err, match) {
                 if (err) return res.serverError(err);
                 if (match) {//issue a token to the user
-                    JwtService.issueToken({ user_id: user.id }, user).then((token) => {
+                    JwtService.issueToken({ user_id: user.id, user_accountType: user.accountType }, user).then((token) => {
+                        //call afterSignin function
+                        sails.config.jsonWebToken.afterSignin(user)
                         return res.json({ user: user, token: token });
                     }).catch((err) => {
                         return res.serverError(err);
@@ -47,27 +71,33 @@ module.exports = {
         });
     },
 
+
+    /**
+     * Controller action called when signing up
+     */
     signup: function (req, res) {
-        JwtService.createUser(req).then( user => {
-            JwtService.issueToken({ user_id: user.id }, user).then((token) => {
+        JwtService.createUser(req.body).then(user => {
+            JwtService.issueToken({ user_id: user.id, user_accountType: user.accountType }, user).then((token) => {
+                //after signup function
+                sails.config.jsonWebToken.afterSignup(user)
                 return res.json({ user: user, token: token });
             }).catch((err) => {
                 return res.json(401, {
-                        err: {
-                            status: 'danger',
-                            message: 'Unable to create token for new account !!, account created'
-                        },
-                        message: err
-                    });
+                    err: {
+                        status: 'danger',
+                        message: 'Unable to create token for new account !!, account created'
+                    },
+                    message: err
+                });
             });
-        }).catch( err => {
+        }).catch(err => {
             return res.json(400, {
-                        err: {
-                            status: 'danger',
-                            message: 'Unable to create account'
-                        },
-                        message: err
-                    });
+                err: {
+                    status: 'danger',
+                    message: 'Unable to create account'
+                },
+                message: err
+            });
         })
     }
 };
--- api/services/JwtService.js
+++ api/services/JwtService.js
@@ -10,7 +10,7 @@ module.exports.issueToken = function (payload, user) {
         //request for a new one
         jwt.sign(payload, sails.config.jsonWebToken.token_secret, sails.config.jsonWebToken.options, function (err, token) {
           if (err) reject(err);
-          User.update({id: user.id},{token: token}).then(data => resolve(token)).catch(error => reject(error))
+          User.update({ id: user.id }, { token: token }).then(data => resolve(token)).catch(error => reject(error))
         });
       } else {//if its still valid and not expired
         resolve(user.token);
@@ -18,6 +18,8 @@ module.exports.issueToken = function (payload, user) {
     });
   });
 };
+
+
 
 /**
  * Returns a promise after verifing the token passed to it. If successful promise contains decoded payload
@@ -31,28 +33,45 @@ module.exports.verifyToken = function (token) {
   });
 };
 
-module.exports.createUser = function (req){
-  return new Promise ((resolve, reject) => {
-        // Validate request paramaters
-        if (!req.body.email || !req.body.password) {
-            reject('email or password parameter(s) missing')
-        }
 
-        //new user object
-        var newUser = {
-            email: req.body.email,
-            password: req.body.password,
-            active: sails.config.jsonWebToken.default_account_status
-        };
+/**
+ * Creates a new user
+ */
+module.exports.createUser = function (body) {
+  return new Promise((resolve, reject) => {
+    var newUser = {}
 
-        if(req.body.accountType){
-          newUser['accountType'] = req.body.accountType
-        }
+    if (sails.config.jsonWebToken.authType == "email") {
+      // Validate request paramaters
+      if (!body.email || !body.password) {
+        reject('email or password parameter(s) missing')
+      }
 
-        User.create(newUser).then((user) => {
-            resolve(user)
-        }).catch((err) => {
-            reject(err)
-        });
+      newUser["email"] = body.email
+    } else {
+      // Validate request paramaters
+      if (!body.username || !body.password) {
+        reject('username or password parameter(s) missing')
+      }
+
+      newUser["username"] = body.username
+    }
+
+
+    //new user object
+    var newUser = {
+      password: body.password,
+      active: sails.config.jsonWebToken.default_account_status
+    };
+
+    if (body.accountType) {
+      newUser["accountType"] = body.accountType
+    }
+
+    User.create(newUser).then((user) => {
+      resolve(user)
+    }).catch((err) => {
+      reject(err)
+    });
   });
 }
