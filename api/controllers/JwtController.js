5d04a32dfb59d5a0ad3357be777645f8e3e96233
Robophil <oghenerobo@gmail.com>
2017-04-03 12:27:52 +0100

    updated policy to use sails formal response

--- api/controllers/JwtController.js
+++ api/controllers/JwtController.js
@@ -11,24 +11,14 @@ module.exports = {
         if (sails.config.jsonWebToken.authType == "email") {
             // Validate request paramaters. If email and password exist
             if (!req.body.email || !req.body.password) {
-                return res.json(401, {
-                    err: {
-                        status: 'danger',
-                        message: 'Email or password not found'
-                    }
-                });
+                return res.badRequest('Email or password not found')
             }
 
             findCriteria["email"] = req.body.email
         } else {
             // Validate request paramaters. If username and password exist
             if (!req.body.username || !req.body.password) {
-                return res.json(401, {
-                    err: {
-                        status: 'danger',
-                        message: 'Username or password not found'
-                    }
-                });
+                return res.badRequest('Username or password not found')
             }
 
             findCriteria["username"] = req.body.username
@@ -37,37 +27,27 @@ module.exports = {
         //find email matching user
         User.findOne(findCriteria).then((user) => {
             if (!user) {
-                return res.json(401, {
-                    err: {
-                        status: 'warn',
-                        message: 'User not found'
-                    }
-                });
+                return res.badRequest('User not found')
             }
 
             //validate password (password, callback)
             user.isPasswordValid(req.body.password, function (err, match) {
-                if (err) return res.serverError(err);
+                if (err) return res.negotiate(err);
                 if (match) {//issue a token to the user
                     JwtService.issueToken({ user_id: user.id, user_accountType: user.accountType }, user).then((token) => {
                         //call afterSignin function
                         sails.config.jsonWebToken.afterSignin(user)
                         return res.json({ user: user, token: token });
                     }).catch((err) => {
-                        return res.serverError(err);
+                        return res.negotiate(err);
                     })
 
                 } else {
-                    return res.json(401, {
-                        err: {
-                            status: 'danger',
-                            message: 'Invalid password'
-                        }
-                    });
+                    return res.badRequest('Invalid password')
                 }
             });
         }).catch((err) => {
-            return res.serverError(err);
+            return res.negotiate(err);
         });
     },
 
@@ -82,22 +62,10 @@ module.exports = {
                 sails.config.jsonWebToken.afterSignup(user)
                 return res.json({ user: user, token: token });
             }).catch((err) => {
-                return res.json(401, {
-                    err: {
-                        status: 'danger',
-                        message: 'Unable to create token for new account !!, account created'
-                    },
-                    message: err
-                });
+                return res.negotiate(err)
             });
         }).catch(err => {
-            return res.json(400, {
-                err: {
-                    status: 'danger',
-                    message: 'Unable to create account'
-                },
-                message: err
-            });
+            return res.negotiate(err)
         })
     }
 };
